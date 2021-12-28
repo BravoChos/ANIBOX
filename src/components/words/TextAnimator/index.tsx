9e491c3b7e04dede73a5d1a6b3a5c64ae2889b5a
Song Cho <iamsongcho@gmail.com>
2021-12-28 18:46:32 +0900

    implement textAnimator component

---
+++ src/components/words/TextAnimator/index.tsx
@@ -0,0 +1,128 @@
+import React, {useEffect} from 'react';
+import {Text, TouchableOpacity, View, StyleSheet, Animated} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+
+import {RootStackParamList} from '../../../screens/types';
+const TextAnimator = ({content, duration, onFinish, style, textStyle}) => {
+  const navigation = useNavigation<RootStackParamList>();
+  let animatedValues: [] = [];
+  let animatedFooterValue = new Animated.Value(0);
+
+  const textArr = content.trim().split('\n');
+  textArr.forEach((_, i) => {
+    animatedValues[i] = new Animated.Value(0);
+  });
+
+  const animated = (toValue = 1) => {
+    const animations = textArr.map((_, i) => {
+      return Animated.timing(animatedValues[i], {
+        toValue,
+        duration: duration,
+        useNativeDriver: true,
+      });
+    });
+
+    Animated.stagger(
+      toValue === 0 ? duration / 40 : duration / 5,
+      toValue === 0 ? animations.reverse() : animations,
+    ).start(() => {
+      if (toValue) {
+        animatedFooters(1);
+      } else {
+        animated(1);
+      }
+    });
+  };
+
+  const animatedFooters = (toValue = 1) => {
+    Animated.timing(animatedFooterValue, {
+      toValue,
+      duration: duration,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  useEffect(() => {
+    animated();
+  }, []);
+
+  return (
+    <>
+      <View style={[style, styles.textWrapper]}>
+        {textArr.map((word, index) => {
+          return (
+            <Animated.Text
+              key={`${word}-${index}`}
+              style={[
+                textStyle,
+                {
+                  opacity: animatedValues[index],
+                  transform: [
+                    {
+                      translateY: Animated.multiply(
+                        animatedValues[index],
+                        new Animated.Value(-5),
+                      ),
+                    },
+                  ],
+                },
+              ]}>
+              {word}
+              {`${index < textArr.length ? ' ' : ''}`}
+            </Animated.Text>
+          );
+        })}
+      </View>
+      <View style={styles.footer}>
+        <TouchableOpacity
+          onPress={() => {
+            animated(0);
+            animatedFooters(0);
+          }}>
+          <Animated.Text
+            style={[
+              {
+                opacity: animatedFooterValue,
+              },
+              styles.buttonStyle,
+            ]}>
+            Reset
+          </Animated.Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Animated.Text
+            style={[
+              {
+                opacity: animatedFooterValue,
+              },
+              styles.buttonStyle,
+            ]}>
+            Go back
+          </Animated.Text>
+        </TouchableOpacity>
+      </View>
+    </>
+  );
+};
+
+export default TextAnimator;
+
+const styles = StyleSheet.create({
+  footer: {
+    marginTop: 24,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  textWrapper: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  buttonStyle: {
+    borderWidth: 1,
+    borderRadius: 12,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+  },
+});
--- src/screens/WordsScreen/index.tsx
+++ src/screens/WordsScreen/index.tsx
@@ -1,11 +1,31 @@
 import * as React from 'react';
-import {StatusBar, Text, View, StyleSheet} from 'react-native';
+import {
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+} from 'react-native';
+
+import TextAnimator from '../../components/words/TextAnimator';
 
 const WordsScreen = () => {
+  const _onFinish = () => {
+    // Alert.alert('Animation text', 'Yeah!! It is woriking!');
+  };
   return (
     <View style={styles.container}>
       <StatusBar hidden />
-      <Text>Words Screen</Text>
+
+      <TextAnimator
+        content={
+          'I hope to live with a conscience clear \n until my dying day \n And yet like the windblown leaf \n I have suffered \n                                          \n I must love all those close to death \n with a heart that sings of the stars. \n And take the path \n I have been called to walk \n Even to night, \n the stars are being ruffled by the wind \n                                                \n "Prologue", by Yun, Dong-ju'
+        }
+        textStyle={styles.textStyle}
+        style={styles.containerStyle}
+        duration={1400}
+        onFinish={_onFinish}
+      />
     </View>
   );
 };
@@ -14,16 +34,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    // paddingTop: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
     padding: 8,
   },
   containerStyle: {},
   textStyle: {
-    fontSize: 28,
+    fontSize: 14,
     fontWeight: 'bold',
     fontFamily: 'Menlo',
     marginBottom: 14,
+  },
+  buttonStyle: {
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    borderWidth: 1,
+    borderRadius: 12,
   },
 });
 export default WordsScreen;
