b22713ea466c4bc836b7b45132f5cbd031f85e22
Song Cho <iamsongcho@gmail.com>
2022-01-07 01:32:12 +0900

    link the two animated FlatList

--- src/screens/GalleryScreen/index.tsx
+++ src/screens/GalleryScreen/index.tsx
@@ -1,5 +1,13 @@
-import React, {useEffect, useState} from 'react';
-import {StatusBar, Text, FlatList, Image, View, StyleSheet} from 'react-native';
+import React, {useEffect, useState, useRef} from 'react';
+import {
+  StatusBar,
+  Text,
+  FlatList,
+  Image,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import {width, height} from '../../styles';
 import {API_KEY} from './config';
 // Pexels.com API
@@ -7,7 +15,7 @@ const API_URL =
   'https://api.pexels.com/v1/search?query=nature&orientation=portrait&size=small&per_page=20';
 
 import {getImagesFromPixcels} from '../../api/gallery';
-const ITEM_SIZE = 80;
+const IMAGE_SIZE = 80;
 const SPACING = 10;
 
 const GalleryScreen = () => {
@@ -20,6 +28,29 @@ const GalleryScreen = () => {
     });
   }, []);
 
+  const topRef = useRef();
+  const thumbRef = useRef();
+  const [activeIndex, setActiveIndex] = useState(0);
+  const scrollToActiveIndex = index => {
+    setActiveIndex(index);
+    // scroll flatlists
+    topRef?.current?.scrollToOffset({
+      offset: index * width,
+      animated: true,
+    });
+
+    if (index * (IMAGE_SIZE + SPACING) - IMAGE_SIZE / 2 > width / 2) {
+      thumbRef?.current?.scrollToOffset({
+        offset: index * (IMAGE_SIZE + SPACING) - width / 2 + IMAGE_SIZE / 2,
+        animated: true,
+      });
+    } else {
+      thumbRef?.current?.scrollToOffset({
+        offset: 0,
+        animated: true,
+      });
+    }
+  };
   if (!images) {
     return (
       <View>
@@ -34,13 +65,19 @@ const GalleryScreen = () => {
       {images ? (
         <>
           <FlatList
+            ref={topRef}
             horizontal
             pagingEnabled
             showsHorizontalScrollIndicator={false}
+            onMomentumScrollEnd={ev => {
+              scrollToActiveIndex(
+                Math.floor(ev.nativeEvent.contentOffset.x / width),
+              );
+            }}
             data={images}
             keyExtractor={item => item.id.toString()}
-            renderItem={({item}) => {
-              console.log(item?.src?.portrait, 1111);
+            renderItem={({item, index}) => {
+              // console.log(item?.src?.portrait, 1111);
               // return <View></View>;
               return (
                 <View style={{width, height}}>
@@ -58,30 +95,34 @@ const GalleryScreen = () => {
             }}
           />
           <FlatList
+            ref={thumbRef}
             horizontal
             // pagingEnabled
             showsHorizontalScrollIndicator={false}
             data={images}
             keyExtractor={item => item.id.toString()}
             contentContainerStyle={{paddingHorizontal: SPACING}}
-            style={{position: 'absolute', bottom: ITEM_SIZE}}
-            renderItem={({item}) => {
+            style={{position: 'absolute', bottom: IMAGE_SIZE}}
+            renderItem={({item, index}) => {
               // console.log(item?.src?.portrait, 1111);
               // return <View></View>;
               return (
-                // <View style={{width, height}}>
-                <Image
-                  source={{uri: item?.src?.portrait}}
-                  style={{
-                    // borderWidth: 1,
-                    // position: 'absolute',
-                    width: ITEM_SIZE,
-                    height: ITEM_SIZE,
-                    borderRadius: 12,
-                    marginRight: SPACING,
-                  }}
-                />
-                // </View>
+                <TouchableOpacity onPress={() => scrollToActiveIndex(index)}>
+                  <Image
+                    source={{uri: item?.src?.portrait}}
+                    style={{
+                      // borderWidth: 1,
+                      // position: 'absolute',
+                      width: IMAGE_SIZE,
+                      height: IMAGE_SIZE,
+                      borderRadius: 12,
+                      marginRight: SPACING,
+                      borderWidth: 2,
+                      borderColor:
+                        activeIndex === index ? '#fff' : 'transparent',
+                    }}
+                  />
+                </TouchableOpacity>
               );
             }}
           />
