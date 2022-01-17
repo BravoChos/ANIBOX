a35d0442aa6eec9e2ad73f9fa75fb71ff62283e9
Song Cho <iamsongcho@gmail.com>
2022-01-17 19:54:00 +0900

    implement styles in RowButton

--- src/components/common/RowButton/index.tsx
+++ src/components/common/RowButton/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {TouchableOpacity, Text, View} from 'react-native';
+import {TouchableOpacity, Text, View, StyleSheet} from 'react-native';
 
 import AntDesign from 'react-native-vector-icons/dist/AntDesign';
 
@@ -8,18 +8,22 @@ const SPACING = 8;
 export default ({text, onPress}: {text: string; onPress?: () => void}) => {
   return (
     <TouchableOpacity onPress={onPress}>
-      <View
-        style={{
-          flexDirection: 'row',
-          padding: SPACING * 2,
-          justifyContent: 'space-between',
-          borderColor: 'rgba(0,0,0,0.1)',
-          borderBottomWidth: 1,
-          borderTopWidth: 1,
-        }}>
-        <Text style={{fontSize: 14}}>{text}</Text>
+      <View style={styles.btnContainer}>
+        <Text style={styles.textSize}>{text}</Text>
         <AntDesign name="arrowright" color="rgba(0,0,0,0.8)" size={14} />
       </View>
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  btnContainer: {
+    flexDirection: 'row',
+    padding: SPACING * 2,
+    justifyContent: 'space-between',
+    borderColor: 'rgba(0,0,0,0.1)',
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
+  },
+  textSize: {fontSize: 14},
+});
